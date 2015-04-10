bd596ed06737bc9cff13d523b6a9d5e60227c388
root <root@localhost.localdomain>
2015-04-10 09:21:37 -0400

    Changed approach and data structures. Framework for attacks is set up

--- botnetbuilder.py
+++ botnetbuilder.py
@@ -5,11 +5,11 @@
 #
 # This tool is intended to automate the steps a crude attacker might take 
 # upon gaining access to a subnet. We begin by performing an nmap scan to
-# discover hosts and services. We then exhaustively deploy exploits until 
-# we gain a session. We then install persistent access tools and save the 
-# credentials
+# discover hosts and services. Then we use the msfconsole to deploy 
+# exploits in the background according to premade .rc files 
 
 import nmap
+import os
 import sys
 
 # Performs a scan on a given subnet.
@@ -20,15 +20,15 @@ def scan(subnet):
   print "Starting scan on subnet " + subnet + "...."
   nm = nmap.PortScanner()
   nm.scan(hosts=subnet, arguments='-sS')
+  host_info = {}
 
   #print out scan results#
-  live_hosts = []
   for host in nm.all_hosts():
     
     #print and track live hosts#
     if nm[host].state() == 'up':
-      live_hosts.append(nm[host])
       print "FOUND LIVE HOST: " + host
+      host_info[host] = []
 
       #list all service info#
       #iterate over all protocols and ports#
@@ -38,18 +38,42 @@ def scan(subnet):
 
       	for port in lport:
       		print('port: %s \tstate: %s \tname: %s' % (port, nm[host][proto][port]['state'], nm[host][proto][port]['name']))
+      		host_info[host].append(nm[host][proto][port]['name'])
 
+  return host_info
 
-  return live_hosts
+#Given a path to an .rc file which lists msfconsole commands,#
+#deploy the attack and pause if attack is successful#
+def deploy_attack(attack_file):
+	#TODO: IMPLEMENT THIS#
 
-# Given a list of python-nmap objects, #
-# parse the msfconsole output and return #
-# a list of exploits for us to try#
-def get_exploit_list(live_hosts):
+#Given a dictionary of python-nmap scan results#
+#Iterate through all .rc files in the attacks/ #
+#folder and deploy them using msfconsole. Stop #
+#once an attack is successful # 
+def attack(scan_info):
 
-	os_strings = ['linux', 'windows']
+	for host in scan_info.keys():
+		print "BEGINNING ATTACK ON: " + host
 
+		#first, look for .rc file matching the service#
+		for name in scan_info[host]:
+		
+			print('Searching for matching attack files for service: %s' % name)
 
+			#Search 'attacks' folder for matching service folder#
+			path = "attacks/"+name+"/"
+			if(os.path.isdir(path)):
+
+				#get .rc files from directory#
+				attack_files = [ f for f in os.listdir(path) if (os.path.isfile(join(path,f)) and (".rc" in f))]
+				print('Found %i matching attacks for service" %s' % len(attack_files) , name)
+
+				#call helper func to deploy attacks#
+				for f in attack_files:
+					deploy_attack(f)
+			else:
+				print('No matching attacks for service: %s' % name)
 
 if __name__ == "__main__":
 
@@ -60,6 +84,6 @@ if __name__ == "__main__":
     sys.exit()
 
   subnet = sys.argv[1]
-  live_hosts = scan(subnet)
-  exploits = get_exploit_list(live_hosts)
+  scan_info = scan(subnet)
+  attack(scan_info)
     
